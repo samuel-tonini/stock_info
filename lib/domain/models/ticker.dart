a7a1d0f0a3fca8623300082e140abb9a3f9c9795
Samuel Tonini <samueltonini@msn.com>
2021-09-09 00:11:13 -0300

    feat: add ticker validation

--- lib/domain/models/ticker.dart
+++ lib/domain/models/ticker.dart
@@ -2,7 +2,11 @@ import 'package:equatable/equatable.dart';
 
 class Ticker extends Equatable {
   final String abreviation;
-  Ticker(this.abreviation);
+  Ticker(this.abreviation) {
+    if (abreviation.isEmpty) {
+      throw ArgumentError.value('', 'abreviation', 'Ticker abreviation must to have a value');
+    }
+  }
 
   @override
   List<Object?> get props => [abreviation];
