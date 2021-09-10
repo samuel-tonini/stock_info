b6c4ec59c9c958ba5dac381dededdf57a74075d7
Samuel Tonini <samueltonini@msn.com>
2021-09-10 00:48:29 -0300

    feat: improve company info ui

--- lib/presentation/ui/company_info/company_info_page.dart
+++ lib/presentation/ui/company_info/company_info_page.dart
@@ -17,39 +17,61 @@ class CompanyInfoPage extends StatelessWidget {
         builder: (context, snapshot) {
           Widget body;
 
+          final textTheme = Theme.of(context).textTheme;
+
           if (snapshot.hasError) {
             body = Center(
-              child: Text('Error'),
+              child: Text(
+                'Error',
+                style: textTheme.headline4,
+              ),
             );
           } else if (snapshot.hasData && snapshot.data != null) {
-            body = Center(
+            body = Padding(
+              padding: const EdgeInsets.all(16.0),
               child: SingleChildScrollView(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Address'),
-                    Text(snapshot.data?.address ?? ''),
-                    Text('${snapshot.data?.city ?? ''}, ${snapshot.data?.state ?? ''}'),
-                    Text(snapshot.data?.zip ?? ''),
-                    Text(snapshot.data?.country ?? ''),
-                    Text('Phone'),
-                    Text(snapshot.data?.phone ?? ''),
-                    Text('Site'),
-                    Text(snapshot.data?.webSite ?? ''),
-                    Text('Sector'),
-                    Text('${snapshot.data?.sector ?? ''}, ${snapshot.data?.industry ?? ''}'),
-                    Text('Description'),
-                    Text(snapshot.data?.description ?? ''),
+                    Text('Address', style: textTheme.subtitle2),
+                    SizedBox(width: 8.0, height: 8.0),
+                    Text(snapshot.data?.address ?? '', style: textTheme.bodyText1),
+                    Text('${snapshot.data?.city ?? ''}, ${snapshot.data?.state ?? ''}', style: textTheme.bodyText1),
+                    Text(snapshot.data?.zip ?? '', style: textTheme.bodyText1),
+                    Text(snapshot.data?.country ?? '', style: textTheme.bodyText1),
+                    SizedBox(width: 16.0, height: 16.0),
+                    Text('Phone', style: textTheme.subtitle2),
+                    SizedBox(width: 8.0, height: 8.0),
+                    Text(snapshot.data?.phone ?? '', style: textTheme.bodyText1),
+                    SizedBox(width: 16.0, height: 16.0),
+                    Text('Site', style: textTheme.subtitle2),
+                    SizedBox(width: 8.0, height: 8.0),
+                    Text(snapshot.data?.webSite ?? '', style: textTheme.bodyText1),
+                    SizedBox(width: 16.0, height: 16.0),
+                    Text('Sector', style: textTheme.subtitle2),
+                    SizedBox(width: 8.0, height: 8.0),
+                    Text('${snapshot.data?.sector ?? ''}, ${snapshot.data?.industry ?? ''}',
+                        style: textTheme.bodyText1),
+                    SizedBox(width: 16.0, height: 16.0),
+                    Text('Description', style: textTheme.subtitle2),
+                    SizedBox(width: 8.0, height: 8.0),
+                    Text(snapshot.data?.description ?? '', style: textTheme.bodyText1),
                   ],
                 ),
               ),
             );
           } else {
-            body = Column(
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(width: 16.0, height: 16.0),
-                Text('Wait...'),
-              ],
+            body = Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(width: 16.0, height: 16.0),
+                  Text('Wait...', style: textTheme.headline6),
+                ],
+              ),
             );
           }
 
