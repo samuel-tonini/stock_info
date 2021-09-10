5a253ee8d467c7e7a0f72156d37431caa6115b1d
Samuel Tonini <samueltonini@msn.com>
2021-09-10 09:32:30 -0300

    test: add historical price data into company info

--- test/presentation/presenters/getx_company_info_presenter_test.dart
+++ test/presentation/presenters/getx_company_info_presenter_test.dart
@@ -4,17 +4,30 @@ import 'package:mockito/mockito.dart';
 import 'package:stock_info/domain/models/models.dart';
 import 'package:stock_info/domain/usecases/usecases.dart';
 
+import 'package:stock_info/presentation/models/models.dart';
 import 'package:stock_info/presentation/presenters/presenters.dart';
 
 main() {
   late GetxCompanyInfoPresenter sut;
   late LoadCompanyInfoSpy loadCompanyInfo;
+  late LoadStockPriceHistorySpy loadStockPriceHistory;
   late Ticker ticker;
   late CompanyInfo companyInfo;
+  late List<HistoricalStockPrice> prices;
+  late List<HistoricalStockPriceViewModel> pricesViewModel;
 
   setUp(() {
     loadCompanyInfo = LoadCompanyInfoSpy();
+    loadStockPriceHistory = LoadStockPriceHistorySpy();
     ticker = Ticker('AAPL');
+    prices = [
+      HistoricalStockPrice(ticker: 'AAPL', at: DateTime.fromMillisecondsSinceEpoch(1587475800 * 1000), price: 272.37),
+      HistoricalStockPrice(ticker: 'AAPL', at: DateTime.fromMillisecondsSinceEpoch(1587476700 * 1000), price: 273.19),
+      HistoricalStockPrice(ticker: 'AAPL', at: DateTime.fromMillisecondsSinceEpoch(1587477600 * 1000), price: 272.31),
+      HistoricalStockPrice(ticker: 'AAPL', at: DateTime.fromMillisecondsSinceEpoch(1587478500 * 1000), price: 270.65),
+      HistoricalStockPrice(ticker: 'AAPL', at: DateTime.fromMillisecondsSinceEpoch(1587479400 * 1000), price: 269.25),
+    ];
+    pricesViewModel = prices.map((price) => HistoricalStockPriceViewModel(price)).toList();
     companyInfo = CompanyInfo(
       ticker: 'AAPL',
       address: 'One Apple Park Way',
@@ -32,14 +45,17 @@ main() {
     sut = GetxCompanyInfoPresenter(
       ticker: ticker,
       loadCompanyInfo: loadCompanyInfo,
+      loadStockPriceHistory: loadStockPriceHistory,
     );
     when(loadCompanyInfo(ticker)).thenAnswer((_) async => companyInfo);
+    when(loadStockPriceHistory(ticker: ticker, priceInterval: sut.priceInterval)).thenAnswer((_) async => prices);
   });
 
-  test('Should call LoadCompanyInfo', () async {
+  test('Should call LoadCompanyInfo and LoadStockPriceHistory', () async {
     await sut.load();
 
     verify(loadCompanyInfo(ticker)).called(1);
+    verify(loadStockPriceHistory(ticker: ticker, priceInterval: PriceInterval.oneHour)).called(1);
   });
 
   test('Should emit error if LoadCompanyInfo throws', () async {
@@ -50,8 +66,22 @@ main() {
     await sut.load();
   });
 
+  test('Should emit error if LoadStockPriceHistory throws', () async {
+    when(loadStockPriceHistory(ticker: ticker, priceInterval: PriceInterval.oneHour)).thenThrow(Error());
+
+    expectLater(sut.historicalPriceStream, emitsInOrder([[], emitsError(isA<Error>())]));
+
+    await sut.load();
+  });
+
   test('Should emit null and after the result', () async {
     expectLater(sut.companyInfoStream, emitsInOrder([null, companyInfo]));
+
+    await sut.load();
+  });
+
+  test('Should emit empty and after the result', () async {
+    expectLater(sut.historicalPriceStream, emitsInOrder([[], pricesViewModel]));
 
     await sut.load();
   });
@@ -63,6 +93,27 @@ main() {
 
     expect(sut.title, companyInfo.name);
   });
+
+  test('Should start with one hour interval', () async {
+    expect(sut.priceInterval, PriceInterval.oneHour);
+  });
+
+  test('Should change interval and reload historical data', () async {
+    expect(sut.priceInterval, PriceInterval.oneHour);
+
+    sut.priceInterval = PriceInterval.oneDay;
+
+    expect(sut.priceInterval, PriceInterval.oneDay);
+    verify(loadStockPriceHistory(ticker: ticker, priceInterval: PriceInterval.oneDay)).called(1);
+  });
+
+  test('Should not reload historical data if same interval was provided', () async {
+    expect(sut.priceInterval, PriceInterval.oneHour);
+
+    sut.priceInterval = PriceInterval.oneHour;
+
+    verifyNever(loadStockPriceHistory(ticker: ticker, priceInterval: PriceInterval.oneHour));
+  });
 }
 
 class LoadCompanyInfoSpy extends Mock implements LoadCompanyInfo {
@@ -70,6 +121,18 @@ class LoadCompanyInfoSpy extends Mock implements LoadCompanyInfo {
     return super.noSuchMethod(
       Invocation.method(#call, []),
       returnValue: CompanyInfo.empty(ticker.abreviation),
+    );
+  }
+}
+
+class LoadStockPriceHistorySpy extends Mock implements LoadStockPriceHistory {
+  Future<List<HistoricalStockPrice>> call({
+    required Ticker ticker,
+    required PriceInterval priceInterval,
+  }) async {
+    return super.noSuchMethod(
+      Invocation.method(#call, [], {#ticker: ticker, #priceInterval: priceInterval}),
+      returnValue: <HistoricalStockPrice>[],
     );
   }
 }
